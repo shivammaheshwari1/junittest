88cb476683700a4b231e6184cccc9e3489b0ad13
shivammaheshwari1 <67630535+shivammaheshwari1@users.noreply.github.com>
2020-07-03 15:59:36 +0530

    Update helloTest.java

--- src/test/java/helloTest.java
+++ src/test/java/helloTest.java
@@ -5,7 +5,16 @@ public class helloTest {
 hello hel=new hello();
 @Test
 public void check(){
+System.out.println("test1");
 String result = hel.concat("Hello ", "World");
 Assert.assertEquals("Hello World", result);
+System.out.println(result);
+}
+@Test
+public void checkfail(){
+System.out.println("test2");
+String result1 = hel.concat("Hello ", "World1");
+Assert.assertEquals("Hello World", result);
+System.out.println(result);  
 }
 }
