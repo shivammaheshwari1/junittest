1745f8c92b506e95348f38655e4dde189eee6ad6
shivammaheshwari1 <67630535+shivammaheshwari1@users.noreply.github.com>
2020-07-03 15:24:38 +0530

    Update simpleTest.java

--- src/test/java/simpleTest.java
+++ src/test/java/simpleTest.java
@@ -1,5 +1,5 @@
 import org.junit.jupiter.api.*;
-import static org.junit.Assert.*;
+import org.junit.Assert.*;
  
 public class simpleTest {
   
@@ -7,14 +7,14 @@ public class simpleTest {
     public void isGreaterTest() {
         System.out.println("Test1");
         simple obj1 = new simple();
-        assertTrue("Num 1 is greater than Num 2", obj1.isGreater(4, 3));
+        Assert.assertTrue("Num 1 is greater than Num 2", obj1.isGreater(4, 3));
     }
   
     @Test
     public void isGreaterTestfail() {
         System.out.println("Test2");
         simple obj2 = new simple();
-        assertTrue("Num 1 is greater than Num 2", obj2.isGreater(2, 3));
+        Assert.assertTrue("Num 1 is greater than Num 2", obj2.isGreater(2, 3));
     }
  
 }
