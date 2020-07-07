fa854da8c023cfe38df1ebe6bcab4e445133eb31
shivammaheshwari1 <67630535+shivammaheshwari1@users.noreply.github.com>
2020-07-07 12:26:39 +0530

    Update helloTest.java

--- src/test/java/helloTest.java
+++ src/test/java/helloTest.java
@@ -7,7 +7,7 @@ hello hel=new hello();
 public void check(){
 System.out.println("test1");
 int result = hel.add(1, 2);
-Assert.assertEquals(3, result);
+Assert.assertEquals(4, result);
 System.out.println(result);
 }
 }
